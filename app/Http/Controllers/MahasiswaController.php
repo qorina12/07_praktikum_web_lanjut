9fd1f5e2312e6eba77eff81a824fa73ac7bc37f2
qorina12 <prameswarides12@gmail.com>
2021-03-29 22:47:20 +0700

    Tugas Dua

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -15,7 +15,11 @@ class MahasiswaController extends Controller
     {
         //
         //fungsi eloquent menampilkan data menggunakan pagination
-        $mahasiswas = Mahasiswa::all(); // Mengambil semua isi tabel
+        $mahasiswas = Mahasiswa::where([
+            ['Nama', '!=', Null]
+        ])
+        ->orderBy("Nim", "asc")
+            ->paginate(5); // Mengambil semua isi tabel
         $posts = Mahasiswa::orderBy('Nim', 'desc')->paginate(6);
         return view('mahasiswas.index', compact('mahasiswas'))
         ->with('i', (request()->input('page', 1) - 1) * 5);
--- resources/views/mahasiswas/index.blade.php
+++ resources/views/mahasiswas/index.blade.php
@@ -51,4 +51,5 @@
     </tr>
     @endforeach
     </table>
+    {{$mahasiswas->links('pagination::bootstrap-4')}}
 @endsection
